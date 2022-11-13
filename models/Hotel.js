1abe7b838a7fcf64f52512089c672fd1365c408e
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-13 00:10:41 -0300

    fix-mt-02-23

--- models/Hotel.js
+++ models/Hotel.js
@@ -5,8 +5,8 @@ const schema= new mongoose.Schema({
     name:{type: String, require: true},
     photo:[{type: String, require: true}],
     capacity:{type: Number, require: true},
-    cityId:{type: String, require: true},
-    userId:{type: String, require: true},
+    cityId:{type: mongoose.Types.ObjectId, ref:"cities", require: true},
+    userId:{type: mongoose.Types.ObjectId, ref:"users", require: true},
 })
 
 const Hotel= mongoose.model("hotels", schema)
