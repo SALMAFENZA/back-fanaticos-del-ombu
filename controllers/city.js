33e3137df110740dcc155f6773d3678e2dfc94c7
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-13 14:26:13 -0300

    mt-01-19 w a filter bout continent

--- controllers/city.js
+++ controllers/city.js
@@ -1,20 +1,45 @@
-const City = require('../models/City')
+const City = require("../models/City");
 const controller = {
-
-    create: async ( req, res) =>{
-try{
-    let new_city = await City.create(req.body)
-    res.status(201).json({
-  id: new_city._id,
-  success: true,
-  message:"The City was created successfully"
-    })
-} catch(error){
-    res.status(400).json({
+  create: async (req, res) => {
+    try {
+      let new_city = await City.create(req.body);
+      res.status(201).json({
+        id: new_city._id,
+        success: true,
+        message: "The City was created successfully",
+      });
+    } catch (error) {
+      res.status(400).json({
         success: false,
-        message: error.message
-})
+        message: error.message,
+      });
     }
+  },
+
+  getAll: async (req, res) => {    
+    let query = {};
+
+    if (req.query.continent) {
+      query.continent = req.query.continent;
+      let regExp = new RegExp(`^${query.continent}`, "i");
+      query.continent = regExp;
+    }   
+  
+    try {
+      console.log(query)
+      let city = await City.find(query);
+      res.status(200).json({
+        city: city,
+        success: true,
+        message: "The cities was found successfully",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
-}
-module.exports = controller 
+  },
+
+};
+module.exports = controller;
