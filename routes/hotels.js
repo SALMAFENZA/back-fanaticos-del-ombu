40beb9064e8f3085c796b88b9ea728cf9c61dc40
cornicheli <gabrielcornide.pg@gmail.com>
2022-11-13 14:12:30 -0300

    fix-mt-b2-28

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,6 +2,6 @@ let router = require('express').Router()
 let {create, update} = require('../controllers/hotel')
 
 
-router.post('/create',create)
+router.post('',create)
 router.patch('/:id', update)
 module.exports = router;
