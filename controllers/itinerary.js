ecef5caa3a674c162740c87232083bde88b49e81
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-14 00:51:49 -0300

    Update itinerary.js

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -14,6 +14,40 @@ const controller = {
         message: error.message,
       });
     }
-  }
+  },
+  getallbycity: async (req, res) => {
+    let query = req.query
+
+    if (req.query) {
+      console.log(req.query)
+      query.cityId = req.query.cityId;
+    }   
+
+
+    try {      
+      let itinerary = await Itinerary.find(query);
+
+
+      if (itinerary){    
+
+      res.status(201).json({
+        response: itinerary,
+        success: true,
+        message: "The Itineraries loaded successfully",
+
+      })}else{
+        res.status("404").json({
+          message: "No itineraries could be found",
+          success: false
+        });
+    }
+  } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
 };
+
 module.exports = controller;
