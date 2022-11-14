9593dc545f9ac62e44636c09afe4fd185bc0af65
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-14 03:29:34 -0300

    Update itineraries.js

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,8 +1,9 @@
 let router = require('express').Router()
-let {create , getallbycity,editItinerary} = require('../controllers/itinerary')
+let {create , getallbycity,editItinerary,deleteItinerary} = require('../controllers/itinerary')
 
 router.post('/', create )
 router.get('/',getallbycity)
 router.put('/:id',editItinerary)
+router.delete('/:id',deleteItinerary),
 
 module.exports = router;
