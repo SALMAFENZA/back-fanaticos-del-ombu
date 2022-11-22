4cb7488b1e6106ede3c8b5fd373025e19a5505b6
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-22 05:58:58 -0300

    Update City.js

--- models/City.js
+++ models/City.js
@@ -5,8 +5,7 @@ const schema = new mongoose.Schema({
         continent: {type:String, required:true},
         photo:{type:String, required:true},
         population: {type:Number, required:true},
-        user:{type:String, required:true},
-        userId: {type: mongoose.Types.ObjectId, ref:'users'},
+        userId: {type: mongoose.Types.ObjectId, ref:'users', required: true},
     })
 
     const City= mongoose.model("cities", schema)
