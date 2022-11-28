2a68f24aab30e625f9f77702f67f4b602a97cdc0
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-28 05:21:40 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 let router = require('express').Router()
-let {create , login} = require('../controllers/user')
+let {create , login ,  logout} = require('../controllers/user')
 const schema = require('../schemas/user')
 const Login = require ('../schemas/login')
 const validator = require('../middlewares/validator')
 const accountExistsSignUp = require('../middlewares/accountExistsSignUp');
 const accountExistsSignIn = require('../middlewares/accountExistsSignIn')
+const passport = require ('../config/passport')
 
 router.post('/sign-up', validator(schema), accountExistsSignUp ,create)
 router.patch('/sign-in', validator(Login), accountExistsSignIn , login)
+router.patch('/sign-out', passport.authenticate('jwt', { session: false }), logout)
 module.exports = router; 
