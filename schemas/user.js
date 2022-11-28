14b318b66f9285a2cd9de6392bb912001eaf7d14
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-27 21:30:20 -0300

    Update user.js

--- schemas/user.js
+++ schemas/user.js
@@ -1,65 +1,37 @@
 const joi = require('joi')
 
 const schema = joi.object({
-name:joi   
-    .string()
-    .required() 
-    .max(50)
-    .messages({
-        'any.required': 'Name form required',
-        'string.empty': 'Full name form required',
-        'string.base': 'Name must be a word. Numbers or symbols are not allowed',
-        'string.max': 'Name extremely long',
-    }),
-lastName:joi   
-    .string()
-    .required() 
-    .max(50)
-    .messages({
-        'any.required': 'LastName form required',
-        'string.empty': 'Full LastName form required',
-        'string.base': 'LastName must be a word. Numbers or symbols are not allowed',
-        'string.max': 'LastName extremely long',
-    }),
-    country:joi
-        .string()
-        .required()
-        .min(3)
-        .messages({
-            'any.required': 'Country form required',
-            'string.empty': 'Full Country form required',
-            'string.min': 'Country too short'
+    name: joi.string().required().min(3).max(50).messages({
+        'string.empty': 'name is required',
+        'string.min': 'name must be at least 3 characters long',
+        'string.max': 'name must be at most 50 characters long',
+        'any.required': 'name is required'
         }),
-photo:joi
-    .string()
-    .required()
-    .uri()
-    .messages({
-        'any.required': 'Photo form required',
-        'string.empty': 'Photo form must be filled with a link',
-        'string.uri':'Photo must be a valid Url',
+    lastName: joi.string().required().min(3).max(50).messages({
+        'string.empty': 'lastName is required',
+        'string.min': 'lastName must be at least 3 characters long',
+        'string.max': 'lastName must be at most 50 characters long',
+        'any.required': 'lastName is required'
+        }),
+    role: joi.string().valid('user', 'admin'),
+    photo: joi.string().required().uri().messages({
+        'string.empty': 'photo is required',
+        'string.uri': 'photo must be a valid uri',
+        'any.required': 'photo is required'
+        }),
+    age: joi.number().required().min(18).messages({
+        'number.empty': 'age is required',
+        'number.min': 'age must be at least 18',
+        'any.required': 'age is required'
+        }),
+    email: joi.string().required().email({minDomainSegments: 2}).messages({
+        'string.empty': 'email is required',
+        'string.email': 'email must be a valid email',
+        'any.required': 'email is required'
+        }),
+    password: joi.string().required().messages({
+        'string.empty': 'password is required',
+        'any.required': 'password is required'
     }),
-email:joi
-    .string()
-    .required()
-    .min(1)
-    .email({minDomainSegments:2})
-    .messages({
-        'any.required': 'Email form required',
-        'string.empty': 'Email form must be filled',
-        'string.min': 'Email too short'
-    }),
-password:joi 
-.string()
-.required()
-.min(8)
-.max(30)
-.messages({
-    'any.required': 'Password form required',
-    'string.empty': 'Full password form required',
-    'string.base': 'Password must be a word with a number and a symbol',
-    'string.max': 'Password extremely long',
-}),
-
 })
 module.exports = schema
