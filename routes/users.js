40b265a1d27630dbdf2ae5c2521f37e20898b3e7
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-27 21:30:11 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ let router = require('express').Router()
 let {create} = require('../controllers/user')
 const schema = require('../schemas/user')
 const validator = require('../middlewares/validator')
+const accountExistsSignUp = require('../middlewares/accountExistsSignUp');
 
-router.post('/sign-up', (validator(schema),create))
+router.post('/sign-up', validator(schema), accountExistsSignUp ,create)
 module.exports = router; 
