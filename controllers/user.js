d676d6a5f5d6e653123c0947dc66f60a5b47537d
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-27 21:29:02 -0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,30 @@
 //1ero requiero el modelo q necesito controlar
 const User = require('../models/User')
 //2do defino el obj controller (q va a controlar el modelo)
+const bcryptjs = require('bcryptjs')
+const crypto = require('crypto')
+const accountVerificationEmail = require('../middlewares/accountVerificationEmail')
+const { userSignedUpResponse, userNotFoundResponse, invalidCredentialsResponse, userSignedOutResponse } = require('../response/auth')
+
+
+
+
 const controller = {
 //met para crear un doc User
-create: async (req,res) => {
-try{
-let new_user = await User.create(req.body)
-res.status(201).json({
-    id: new_user._id,
-    success: true,
-    message:"The User was created successfully"
-})
+create: async (req,res , next) => {
+    let { name, lastName, photo, age, email, password } = req.body
+    let role = 'user'
+    let verified = false
+    let logged = false
+    let code = crypto.randomBytes(10).toString('hex')
+    password = bcryptjs.hashSync(password, 10)
+    try{
+ await User.create({ name, lastName, photo, age, email, password, role, verified, logged, code })
+await accountVerificationEmail(email, code)
+return userSignedUpResponse(req, res)
 
 }catch(error){
-res.status(400).json({
-    success: false,
-    message: error.message
-})
+    next(error)
 }
 },
 }
