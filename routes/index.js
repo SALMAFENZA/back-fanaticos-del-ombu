bb1c154d411fbb794b56cffa12c0b7aaf8c2fa23
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-30 12:29:19 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-let router= require('express').Router()
-let users= require('./users')
-let cities= require('./cities')
-let itineraries= require('./itineraries')
+let router = require("express").Router();
+let users = require("./users");
+let cities = require("./cities");
+let itineraries = require("./itineraries");
+let reactions = require("./reactions");
 
-router.use('/api/itineraries',itineraries)
-router.use('/api/auth',users),
-router.use('/api/cities',cities)
+router.use("/api/itineraries", itineraries);
+router.use("/api/auth", users), router.use("/api/cities", cities);
+router.use("/api/reactions", reactions);
 
 module.exports = router;
