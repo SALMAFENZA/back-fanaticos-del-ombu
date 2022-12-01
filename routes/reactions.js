3e2e87157cdf4859a8459dbb09ba0dee42dae754
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-11-30 21:45:17 -0300

    Update reactions.js

--- routes/reactions.js
+++ routes/reactions.js
@@ -4,7 +4,7 @@ const passport = require ('../config/passport')
 
 router.post('/', create )
 router.get('/', getAll)
-router.put('/',  addUserId )
+router.put('/', passport.authenticate('jwt', { session: false }) , addUserId )
 router.delete('/' , passport.authenticate('jwt', { session: false }) ,delUserId )
 
 module.exports = router;
