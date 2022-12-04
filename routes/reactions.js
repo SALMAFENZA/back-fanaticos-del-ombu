0b70d0df18718dd0089e74d1389ba62740e0924d
Salma Fenza <114601059+SALMAFENZA@users.noreply.github.com>
2022-12-04 12:02:11 -0300

    Update reactions.js

--- routes/reactions.js
+++ routes/reactions.js
@@ -1,11 +1,12 @@
 let router = require('express').Router()
-let { create , getAll , deleteReaction , addDelUserId } = require('../controllers/reaction')
+let { create , getAll , deleteReaction , addDelUserId, getAllByUser } = require('../controllers/reaction')
 const passport = require ('../config/passport')
 const validator = require('../middlewares/validator')
 const schema = require ('../schemas/reaction')
 
         router.post('/'  ,  validator(schema) , create )
-        router.get('/',  passport.authenticate('jwt', { session: false })  , getAll)
+        router.get('/?userId',  passport.authenticate('jwt', { session: false })  , getAllByUser)
+        router.get('/',  getAll)
         router.put('/:id', passport.authenticate('jwt', { session: false })  , addDelUserId )
         router.delete('/' , passport.authenticate('jwt', { session: false }), deleteReaction )
 
